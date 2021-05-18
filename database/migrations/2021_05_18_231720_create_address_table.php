38ff26ae75f3959c84d695e257d5ca5ab1d3c328
rochac2lee <cleberli@dgsolutions.com.br>
2021-05-18 20:55:33 -0300

    Criação da migration Address

--- database/migrations/2021_05_18_231720_create_address_table.php
+++ database/migrations/2021_05_18_231720_create_address_table.php
@@ -15,6 +15,13 @@ class CreateAddressTable extends Migration
     {
         Schema::create('address', function (Blueprint $table) {
             $table->id();
+            $table->integer('cep'); //Código do
+            $table->string('uf'); // Sigla do Estado
+            $table->string('cidade'); // Nome da cidade
+            $table->string('bairro'); // Comunidade ou Região de uma cidade
+            $table->string('endereco'); // Logradouro
+            $table->string('numero')->nullable(); //Número da residência caso exista (Não é obrigatório)
+            $table->longText('complemento')->nullable(); // Complemento do endereço (Não é obrigatório)
             $table->timestamps();
         });
     }
